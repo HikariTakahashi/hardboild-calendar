d954e810cb1db183b621ec1c90ecbe0c613a514d
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-03-26 17:17:21 +0900

    add:buttonコンポーネント

---
+++ components/button/Square.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    @click="$emit('click')"
+    class="w-20 h-12 border rounded-full hover:{{ color }}"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+});
+
+defineEmits(["click"]);
+</script>
