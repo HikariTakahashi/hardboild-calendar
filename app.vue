41265b9af355a664bd7042a5dbd2aefb74a7b07c
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-03-28 20:41:27 +0900

    add:metadata

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
 <template>
   <NuxtPage />
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "Schedule Text Generator",
+  ogTitle: "Schedule Text Generator",
+  description: "簡単に予定を共有できるシンプルなアプリです",
+  ogDescription: "簡単に予定を共有できるシンプルなアプリです",
+  ogImage: "public\metadata.png",
+  twitterCard: "summary_large_image",
+});
+</script>
