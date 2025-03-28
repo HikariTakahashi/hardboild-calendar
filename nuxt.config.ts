f39f34f46d1bd6f07012915e901bab7d1a517e77
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-03-28 20:32:05 +0900

    add:favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  // app: {
+  //   head: {
+  //     charset: "utf-16",
+  //     viewport: "width=device-width",
+  //     title: "Schedule",
+  //     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  //     htmlAttrs: {
+  //       lang: "ja",
+  //       prefix: "og: https://ogp.me/ns#",
+  //     },
+  //   },
+  // },
+});
