b735f61f0b1868af13d5555858c4171a36c514e1
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-03-28 20:50:36 +0900

    fix:metadata

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
 <script setup lang="ts">
 useSeoMeta({
   title: "Schedule Text Generator",
-  ogTitle: "Schedule Text Generator",
+  ogTitle: "スケジュールテキスト生成ジェネレータ",
   description: "簡単に予定を共有できるシンプルなアプリです",
   ogDescription: "簡単に予定を共有できるシンプルなアプリです",
   ogImage: "public\metadata.png",
