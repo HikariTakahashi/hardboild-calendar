1c98954fb6aa9401157a03674bdf946be63b5a65
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-03-29 09:45:26 +0900

    fix:metadata

--- app.vue
+++ app.vue
@@ -8,7 +8,7 @@ useSeoMeta({
   ogTitle: "スケジュールテキスト生成ジェネレータ",
   description: "簡単に予定を共有できるシンプルなアプリです",
   ogDescription: "簡単に予定を共有できるシンプルなアプリです",
-  ogImage: "public\metadata.png",
+  ogImage: "public/metadata.png",
   twitterCard: "summary_large_image",
 });
 </script>
