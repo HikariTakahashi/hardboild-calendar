746ce26cf74ec5200d35466603d3f55180e6d9e0
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-04-15 15:42:15 +0900

    add:ViewToday

--- components/CalendarView.vue
+++ components/CalendarView.vue
@@ -8,20 +8,30 @@
     <div class="flex-1 grid grid-cols-7 grid-rows-6 gap-2 h-80vh">
       <!-- 日の表示 -->
       <div
-        v-for="day in days"
-        :key="day.toDateString()"
-        class="flex flex-col items-center justify-center border rounded transition-transform duration-200 hover:-translate-y-1 relative shadow-md"
-        :class="{ 'bg-gray-100 text-gray-400': day.getMonth() !== month }"
+        v-for="dayObj in days"
+        :key="dayObj.date.toDateString()"
+        class="flex flex-col items-center border rounded transition-transform duration-200 hover:-translate-y-1 relative shadow-md"
+        :class="{
+          'bg-gray-100': dayObj.date.getMonth() !== month,
+        }"
         style="min-height: calc(83vh / 6)"
-        @click="openTimeForm(day)"
+        @click="openTimeForm(dayObj.date)"
       >
-        {{ day.getDate() }}
+        <div
+          class="flex items-center justify-center w-8 h-8 mt-1"
+          :class="{
+            'text-gray-500': dayObj.date.getMonth() !== month,
+            'font-bold rounded-full bg-green-600 text-white': dayObj.isToday,
+          }"
+        >
+          {{ dayObj.date.getDate() }}
+        </div>
         <!-- 予定の表示 -->
         <div
-          v-if="times[day.toDateString()]"
-          class="text-sm text-blue-500 text-center mt-1"
+          v-if="times[dayObj.date.toDateString()]"
+          class="text-center mt-1 font-bold text-xs sm:text-base text-blue-500"
         >
-          {{ formatTimeDisplay(day) }}
+          {{ formatTimeDisplay(dayObj.date) }}
         </div>
       </div>
     </div>
@@ -52,5 +62,6 @@ const {
   deleteTime,
   formatTimeDisplay,
   weekDays,
+  isToday,
 } = useCalendar();
 </script>
